994b458044d62d64b30b03405ba5d75139055fcf
Parsa Moh <parsamoh@Parsas-MacBook-Pro.local>
2023-05-13 13:48:25 +0330

    add iran.dat file download

--- xray-install.sh
+++ xray-install.sh
@@ -7,6 +7,11 @@ wget https://downloads.openwrt.org/snapshots/packages/mipsel_24kc/packages/xray-
 opkg install xray-core_1.8.1-1_mipsel_24kc.ipk -d ram
 rm xray-core_1.8.1-1_mipsel_24kc.ipk
 service passwall restart
+
+mkdir -p /tmp/usr/share/xray/
+cd /tmp/usr/share/xray/
+wget https://github.com/bootmortis/iran-hosted-domains/releases/latest/download/iran.dat -O iran.dat
+
 cd /root/
 
 echo "OK !"
