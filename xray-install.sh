0f7fd9f35f2fdd1be7e1c73b3a351c998a4b7a9c
parsa mohammadnezhad <parsamohammadnezhad@gmail.com>
2023-11-03 22:05:58 +0330

    Update xray-install.sh

--- xray-install.sh
+++ xray-install.sh
@@ -3,8 +3,8 @@
 sleep 5
 service passwall stop
 cd /tmp
-wget https://downloads.openwrt.org/snapshots/packages/mipsel_24kc/packages/xray-core_1.8.1-1_mipsel_24kc.ipk
-opkg install xray-core_1.8.1-1_mipsel_24kc.ipk -d ram
+wget https://pxp-adhoc.s3.ir-thr-at1.arvanstorage.ir/xray-core_1.8.1-1_mipsel_24kc.ipk -O xray.ipk
+opkg install xray.ipk -d ram
 rm xray-core_1.8.1-1_mipsel_24kc.ipk
 service passwall restart
 
